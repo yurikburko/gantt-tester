863155322868e5f709164548eec99547bc332fb4
Бурко <Burko@IMDOMAIN>
2025-02-12 22:47:47 +0300

    Added Gantt POC example with KendoReact component. Missed files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,6 @@ export default function Home() {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{' '}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
 
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                     <a
@@ -37,12 +27,24 @@ export default function Home() {
                     </a>
                 </div>
 
-                <div>
+                <h1>Gantt POC Demo implememtations:</h1>
+
+                <div className="flex gap-4 items-center flex-col sm:flex-row">
                     <div>
-                        <Link href={`/kendo/`}>Kendo implementation</Link>
+                        <Link
+                            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                            href={`/kendo/`}
+                        >
+                            Kendo implementation
+                        </Link>
                     </div>
                     <div>
-                        <Link href={`/canvas-konva/`}>Canvas Konva implementation</Link>
+                        <Link
+                            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                            href={`/canvas-konva/`}
+                        >
+                            Canvas Konva implementation
+                        </Link>
                     </div>
                 </div>
             </main>
