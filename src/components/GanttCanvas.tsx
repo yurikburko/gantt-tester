90ef149d00947c7587225ef624aafbde0c2ff29d
yurikburko <yurik_burko@tut.by>
2025-02-25 17:27:41 +0300

    Gantt Canvas implementation: add timeline header on canvas with infinite scrolling.

--- src/components/GanttCanvas.tsx
+++ src/components/GanttCanvas.tsx
@@ -1,29 +1,122 @@
 import { useDivSize } from '@/common/hooks/useBoxSize';
-import React, { FC } from 'react';
-import { Stage, Layer, Circle, Rect, Text } from 'react-konva';
+import {
+    addDays,
+    daysDiff,
+    formatAsDayNameFirstLetter,
+    formatDatesRange,
+    getDayOfWeek,
+    today as getToday,
+} from '@/utils/dateUtility';
+import Konva from 'konva';
+import React, { FC, useState } from 'react';
+import { Stage, Layer, Rect, Text, Group } from 'react-konva';
+
+const today = getToday();
+const zoomLevel = 20;
+
+const getDate = (offset: number) => {
+    // works for both positive and negative diffs thanks to the Math.floor
+    return addDays(today, Math.floor(offset / zoomLevel));
+};
+const getOffset = (date: Date): number => {
+    return Math.floor(daysDiff(today, date) * zoomLevel);
+};
+
+let isDragging = false;
+let lastDraggingX = 0;
 
 const GanttCanvas: FC = () => {
     const [canvasContainerRef, containerHeight, containerWidth] = useDivSize();
 
+    const [todayX, setTodayX] = useState<number>(0);
+
+    // Timeline offsets
+    const startX = -todayX;
+    const endX = startX + containerWidth;
+
+    const periodStart = getDate(startX),
+        periodEnd = getDate(endX),
+        periodStartWeek = addDays(periodStart, -getDayOfWeek(periodStart));
+    let nextWeek;
+
+    const weeks = [];
+    const days = [];
+    for (let currentWeek = periodStartWeek; currentWeek <= periodEnd; currentWeek = nextWeek) {
+        nextWeek = addDays(currentWeek, 7);
+        const lastDateOfWeek = addDays(currentWeek, 6);
+
+        // week
+        weeks.push(
+            <Group key={currentWeek.getTime()} x={getOffset(currentWeek)} y={-1}>
+                <Rect
+                    width={getOffset(nextWeek) - getOffset(currentWeek) + 1}
+                    height={19}
+                    fill={'#F7F7FC'}
+                    stroke={'#EDEDF2'}
+                    // strokeWidth={1} // border width
+                ></Rect>
+                <Text
+                    fontFamily="Arial"
+                    fontSize={11}
+                    text={formatDatesRange(currentWeek, lastDateOfWeek)}
+                    x={2}
+                    y={5}
+                />
+            </Group>
+        );
+
+        // days
+        let nextDay;
+        for (let currentDay = currentWeek, i = 0; i < 7; currentDay = nextDay, i++) {
+            nextDay = addDays(currentDay, 1);
+
+            const dayWidth = getOffset(nextDay) - getOffset(currentDay) + 1;
+            days.push(
+                <Group key={currentDay.getTime()} x={getOffset(currentDay)} y={17}>
+                    <Rect width={dayWidth} height={19} stroke={'#EDEDF2'} strokeWidth={1}></Rect>
+                    <Text
+                        text={formatAsDayNameFirstLetter(currentDay)}
+                        fontSize={11}
+                        x={dayWidth / 2}
+                        y={6}
+                    />
+                </Group>
+            );
+        }
+    }
+
+    const onMouseDown = (e: Konva.KonvaEventObject<MouseEvent>) => {
+        isDragging = true;
+        lastDraggingX = e.evt.clientX;
+    };
+    const onMouseMove = (e: Konva.KonvaEventObject<MouseEvent>) => {
+        if (isDragging) {
+            const deltaScrollX = e.evt.clientX - lastDraggingX;
+            setTodayX(todayX + deltaScrollX);
+            lastDraggingX = e.evt.clientX;
+        }
+    };
+    const onMouseUp = () => {
+        isDragging = false;
+    };
+
     return (
         <div ref={canvasContainerRef} style={{ height: '100%', overflow: 'hidden' }}>
-            <Stage width={containerWidth} height={containerHeight}>
+            <Stage onMouseDown={onMouseDown}
+                    onMouseUp={onMouseUp}
+                    onMouseMove={onMouseMove} 
+                    width={containerWidth}
+                     height={containerHeight}
+            >
                 <Layer>
-                    <Text text="Some text on canvas" fontSize={15} />
-                    <Rect
-                        x={20}
-                        y={50}
-                        width={100}
-                        height={100}
-                        fill="red"
-                        shadowBlur={10}
-                    />
-                    <Circle x={200} y={100} radius={50} fill="green" />
+                    <Group x={todayX} y={0}>
+                        {weeks}
+                        {days}
+                    </Group>
                 </Layer>
             </Stage>
         </div>
-
-    )
-}
+    );
+};
 
 export default GanttCanvas;
---
+++ src/utils/dateUtility.ts
@@ -0,0 +1,134 @@
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
+const ellipses = "\u2026"; // code \u2026 it's ellipses
+const separator = " - ";
+
+const currentCultureSettings = {
+    firstDay: 0,
+} 
+
+const getCurrentDate = (): Date => {
+    return new Date();
+};
+
+const toDayStart = (date: Date, isUTC = false): Date => {
+    if (isUTC) {
+        date.setUTCMilliseconds(0);
+        date.setUTCSeconds(0);
+        date.setUTCMinutes(0);
+        date.setUTCHours(0);
+    } else {
+        date.setMilliseconds(0);
+        date.setSeconds(0);
+        date.setMinutes(0);
+        date.setHours(0);
+    }
+    return date;
+};
+
+export const today = (): Date => {
+    const today = getCurrentDate();
+    toDayStart(today);
+    return today;
+};
+
+const isCurrentYear = (date: Date): boolean => {
+    const current_date = getCurrentDate();
+    const current_year = current_date.getFullYear();
+    const source_year = date.getFullYear();
+    return current_year === source_year;
+};
+
+export const addDays = (date: Date, days: number): Date =>  {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+export const daysDiff = (first: Date, second: Date) => {
+    const firstDate = Date.UTC(first.getFullYear(), first.getMonth(), first.getDate());
+    const secondDate = Date.UTC(second.getFullYear(), second.getMonth(), second.getDate());
+
+    return Math.ceil((secondDate - firstDate) / MS_PER_DAY);
+}
+
+export const getDayOfWeek = (date: Date): number => {
+    return (7 + date.getDay() - currentCultureSettings.firstDay) % 7;
+}
+
+const compareYears = (firstDate: Date, secondDate: Date): boolean => {
+    if (!firstDate && !secondDate) {
+        return true;
+    }
+
+    if (!firstDate || !secondDate) {
+        return false;
+    }
+
+    return firstDate.getFullYear() === secondDate.getFullYear();
+}
+
+const formatAsShort = (date: Date, nullText?: string): string => {
+    if (date) {
+        return date.toLocaleDateString(undefined, {
+            month: 'short',
+            day: '2-digit',
+        });
+    }
+    return nullText || "";
+}
+
+const formatAsMediumFullYear = (date: Date): string => {
+    return date.toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+    })
+}
+
+const formatAsMediumOrShort = (date: Date, alwaysShowYear: boolean = false): string => {
+    if (!date) {
+        return "";
+    }
+
+    return isCurrentYear(date) && !alwaysShowYear ? formatAsShort(date) : formatAsMediumFullYear(date);
+}
+
+export const formatDatesRange = (start: Date, end: Date, options: {alwaysShowYear?: boolean } = {}): string => {
+    const { alwaysShowYear } = { alwaysShowYear: false, ...options };
+
+    if (start && end) {
+        let result;
+        const isCompareYear = compareYears(start, end);
+
+        if (isCompareYear) {
+            result = formatAsShort(start) + separator + formatAsMediumOrShort(end, alwaysShowYear);
+        } else {
+            result = formatAsMediumFullYear(start) + separator + formatAsMediumFullYear(end);
+        }
+
+        return result;
+    }
+
+    if (start) {
+        return formatAsMediumOrShort(start, alwaysShowYear) + separator + ellipses;
+    }
+
+    return ellipses + separator + formatAsMediumOrShort(end, alwaysShowYear);
+}
+
+const formatAsDayNameMin = (date: Date): string => {
+    if (date) {
+        return (date).toLocaleDateString(undefined, {
+            weekday: 'short',
+        })
+    }
+    
+    return "";
+}
+
+export const formatAsDayNameFirstLetter = (date: Date): string => {
+    if (date) {
+        return formatAsDayNameMin(date).charAt(0);
+    }
+    return "";
+}
